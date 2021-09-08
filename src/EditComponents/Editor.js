ddd1da63bc17310eea6003bdfa15495b4d2a3905
Jeongs <kimm.jeongs@gmail.com>
2021-09-09 00:29:58 +0900

    🐛 Fix: change save values on editor at the same time

--- src/EditComponents/Editor.js
+++ src/EditComponents/Editor.js
@@ -16,14 +16,13 @@ export default function Editor({ $target, initialState, onEditing }) {
       this.state.title === '제목 없음' ? '' : this.state.title
     // 리스트에서 새 문서를 추가할 땐 제목 없음으로 들어가지만 편집기 제목에는 빈 값으로 넣어주기
     $editor.querySelector('[name=content]').innerHTML = this.state.content
-
-    this.render()
   }
 
   this.render = () => {
     if (!isInitialize) {
       // 렌더링 초기화
-      $editor.innerHTML = `<input name='title' 
+      $editor.innerHTML = `<input class='edit-input'
+      name='title' 
       type='text' 
       value='${this.state.title}' 
       placeholder='제목 없음'/>
@@ -37,29 +36,24 @@ export default function Editor({ $target, initialState, onEditing }) {
 
   let timerId
   $editor.addEventListener('keyup', (e) => {
-    const { target } = e
-    const $textarea = document.querySelector('.textarea')
-    const selection = window.getSelection().getRangeAt(0)
+    const $textarea = $editor.querySelector('[name=content]')
+    const $input = $editor.querySelector('[name=title]')
+    console.log($input.value)
+    // const selection = window.getSelection().getRangeAt(0)
     // selection.collapse(e.target, 1)
 
-    const name = target.getAttribute('name')
-
-    if (this.state[name] !== undefined) {
-      // 빈문자열 === false
-      const nextState = {
-        ...this.state,
-        [name]: name === 'title' ? target.value : target.innerHTML,
-      }
-
-      if (timerId) clearTimeout(timerId)
-      timerId = setTimeout(async () => {
-        this.state = nextState
-        await onEditing(this.state)
-        console.log('2초 지나고 onEditing 실행')
-        await target.focus()
-        // await setCaret($target)
-        console.log('caret 저장')
-      }, 2000)
+    const nextState = {
+      ...this.state,
+      title: $input.value, // target으로 할 경우 focus가 사라지면 undefined이 들어감
+      content: $textarea.innerHTML,
     }
+
+    if (timerId) clearTimeout(timerId)
+    timerId = setTimeout(async () => {
+      this.state = nextState
+      await onEditing(nextState)
+      // await setCaret($target)
+    }, 1000)
+    // }
   })
 }
